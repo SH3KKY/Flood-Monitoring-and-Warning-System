1cf6103c54819411cc7659676b800fee2268f367
Shekky <94705265+SH3KKY@users.noreply.github.com>
2024-01-27 14:14:50 +0000

    Now prints closest and furthest 10 stations

--- Task1B.py
+++ Task1B.py
@@ -1,3 +1,5 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
-stations_by_distance(build_station_list(),(52.2053, 0.1218))
+new = stations_by_distance(build_station_list(),(52.2053, 0.1218))
+print("The closest 10 stations to the provided coordinates are:", new[:10])
+print("The furthest 10 stations to the provided coordinates are:", new[-10:])
