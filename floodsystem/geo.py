bedb8b876b67ce58d77b5743dd2a2e95d1b02777
Shekky <94705265+SH3KKY@users.noreply.github.com>
2024-01-27 13:54:03 +0000

    stations by distance algorithm updated

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+import haversine
+
+def stations_by_distance(stations, p):
+   """This function calculates the distance between provided stations and a provided coordinate."""
+   point_to_station_dist = [] #Initialise list
+   for station in stations:
+      #print(station.coord)
+      distance = haversine.haversine(p,station.coord)
+      temp = (station.name, distance)
+      point_to_station_dist.append(temp)
+   sorted_by_key(point_to_station_dist,1)
+   print(point_to_station_dist)
+
+    
