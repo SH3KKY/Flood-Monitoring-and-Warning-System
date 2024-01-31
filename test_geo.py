5f87f9ba3e9b078d76b4af11d1315c925af1ba87
Shekky <94705265+SH3KKY@users.noreply.github.com>
2024-01-31 20:46:16 +0000

    Test added for stations_within_radius function

--- test_geo.py
+++ test_geo.py
@@ -1,22 +1,32 @@
 """Unit test for the geo module"""
 
 from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 import random
+import haversine
 from decimal import Decimal, getcontext
-
 
 def test_stations_by_distance():
     """Testing that the input coordinates is a tuple, the output is arranged in distance"""
-
     #Generating random test coordinates
     getcontext().prec = 15
     test_coordinates = (Decimal(random.uniform(-90,90)),Decimal(random.uniform(-180,180)))
+    all_stations = build_station_list()
 
     #run function
-    final = stations_by_distance(build_station_list(),test_coordinates)
+    distance_result = stations_by_distance(all_stations,test_coordinates)
 
     #Testing results
     assert isinstance(test_coordinates, tuple)
-    assert all(final[i][1] <= final[i + 1][1] for i in range(len(final) - 1))
-    assert final[-1][1] == max(station[1] for station in final)
+    assert all(distance_result[i][1] <= distance_result[i + 1][1] for i in range(len(distance_result) - 1))
+    assert distance_result[-1][1] == max(station[1] for station in distance_result)
+
+def test_stations_within_radius():
+    all_stations = build_station_list()
+    test_radius = random.randint(0,100)
+    """Testing whether the function correctly determines whether stations are within specified radii of an origin"""
+    radius_result = stations_within_radius(all_stations,(0,0),test_radius)
+    for station in all_stations:
+        if station.name in radius_result:
+            assert haversine.haversine((0,0),station.coord) < test_radius
