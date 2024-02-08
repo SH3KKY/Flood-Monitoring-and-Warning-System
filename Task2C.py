f5b7700df330595a90af93b24fafbbc89758d191
ojpsharpe <157902453+ojpsharpe@users.noreply.github.com>
2024-02-08 10:06:06 +0000

    changed Task2C to comply with specification

--- Task2C.py
+++ Task2C.py
@@ -1,19 +1,15 @@
-from floodsystem.flood import stations_level_over_threshold
-from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 
-N = 10
+N = 20
 
 stations = build_station_list()
+update_water_levels(stations)
 
 # for this exercise, i will assume that if the relative water level is below the low typical value the station is not at risk
 # so if the relative water level is below 0, it will not be included in data used for the algorithm
 
-update_water_levels(stations)
+output = stations_highest_rel_level(stations, N)
 
-output = stations_level_over_threshold(stations, 0)
-#print(output)
-
-for i in range(1,N+1):
-    print(output[-i][0], output[-i][1])
-    #print(i)
+for i in range(0, len(output)):
+    print(output[i][0], output[i][1])
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -11,3 +11,19 @@ def stations_level_over_threshold(stations, tol):
     out_of_tol_sorted = sorted(out_of_tol, key = lambda x: x[1])
     return out_of_tol_sorted
 
+
+def stations_highest_rel_level(stations, N):
+
+    return_list = []
+
+    output = stations_level_over_threshold(stations, 0)
+    i = 0
+    output.reverse()
+    #print(output)
+    while i < N:
+        return_list.append(output[i])
+        i+=1
+    
+    #print(return_list)
+
+    return return_list
